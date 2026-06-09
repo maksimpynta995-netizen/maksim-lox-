b228c2578b05bf23c18654d216a559612d605131
user <my-email@example.com>
2026-06-09 08:19:37 +0300

    pepeshenel

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace испро
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
